0d64312ed753b032bd49ae676d7d198a055310cb
dahn0209 <dahn0209@gmail.com>
2022-11-11 01:50:55 -0500

    fixed the css image slider

--- client/components/privateEvents.js
+++ client/components/privateEvents.js
@@ -3,6 +3,9 @@ import {fetchHomepageImages} from '../store/homePageImages'
 import {connect} from 'react-redux'
 // import './homePageImages.css'
 import './privateEvents.css'
+import Slider from 'react-slick'
+import './slick.css'
+import './slick-theme.css'
 
 class PrivateEvents extends React.Component {
   componentDidMount() {
@@ -11,8 +14,42 @@ class PrivateEvents extends React.Component {
 
   render() {
     const homePageImages = this.props.homePageImages
+    const settings = {
+      dots: true,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      initialSlide: 0,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    }
     return (
-      <section className="privateEventsContainer">
+      <Slider className="section" {...settings}>
         {homePageImages.map(homePageImage => {
           return (
             <img
@@ -22,7 +59,7 @@ class PrivateEvents extends React.Component {
             />
           )
         })}
-      </section>
+      </Slider>
     )
   }
 }
