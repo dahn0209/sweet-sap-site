4888e0ae9d3fbeda4e3848f691374ce8f0aeb6fe
dahn0209 <dahn0209@gmail.com>
2022-11-16 04:10:40 -0500

    fit css for homepageImage by adding image description

--- client/components/homePageImages.js
+++ client/components/homePageImages.js
@@ -18,17 +18,35 @@ class HomePageImages extends React.Component {
               <div
                 className="homePageImagesGridItem"
                 key={homePageImage.id}
-                style={{backgroundImage: `url(${homePageImage.imageUrl})`}}
-              />
+                // style={
+                //   {
+                //     backgroundImage:`url(${homePageImage.imageUrl})`
+                //   }
+                // }
+              >
+                <img src={homePageImage.imageUrl} />
+              </div>
             )
           }
           return (
             <div
               className="homePageImagesGridItem"
               key={homePageImage.id}
-              style={{backgroundImage: `url(${homePageImage.imageUrl})`}}
+              // style={
+              //   {
+              //     backgroundImage:`url(${homePageImage.imageUrl})`
+              //   }
+              // }
             >
-              <h2>{homePageImage.description}</h2>
+              <img src={homePageImage.imageUrl} />
+
+              <div className="homePageDescription">
+                <span>{homePageImage.description}</span>
+              </div>
+
+              {/* <span className='homePageDescription'>
+              {homePageImage.description}
+            </span> */}
             </div>
           )
         })}
