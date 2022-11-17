060a075266b4bb1f3652340c6d4637759b4daa7b
dahn0209 <dahn0209@gmail.com>
2022-11-16 19:43:43 -0500

    working on editing login and signup

---
+++ script/listOfUsers.js
@@ -0,0 +1,47 @@
+// array of users
+const users = [
+  {
+    firstName: 'Glenn',
+    lastName: 'Rhee',
+    email: 'grhee@gmail.com',
+    password: 'imalive',
+    address: '13598 East WestGate Drive, Odon, IN 47562'
+  },
+  {
+    firstName: 'Maggie',
+    lastName: 'Greene',
+    email: 'mgreene@gmail.com',
+    password: 'glennsalive',
+    address: '13909 Smoketown Road, Woodbridge, VA 22192'
+  },
+  {
+    firstName: 'Rick',
+    lastName: 'Grimes',
+    email: 'rgrime@gmail.com',
+    password: 'ishetho',
+    address: '140 7th Ave South, St Petersburg, FL 33701'
+  },
+  {
+    firstName: 'Daryl',
+    lastName: 'Dixon',
+    email: 'ddixon@gmail.com',
+    password: 'maybenot',
+    address: '14188 E. Briarwood Ave, Centennial, CO 80112'
+  },
+  {
+    firstName: 'Tyler',
+    lastName: 'Ribbon',
+    email: 'tribbon@gmail.com',
+    password: 'whoareyou',
+    address: '1478 Midway Road, Menasha, WI 54952'
+  },
+  {
+    firstName: 'Hugo',
+    lastName: 'First',
+    email: 'hfirst@gmail.com',
+    password: 'somepassword',
+    address: '1504 Springhill Avenue, Mobile, AL 36604'
+  }
+]
+
+module.exports = users
