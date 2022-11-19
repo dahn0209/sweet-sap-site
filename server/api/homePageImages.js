4e46a94b694784579d3652e35604a474a7496b0c
dahn0209 <dahn0209@gmail.com>
2022-11-18 21:45:33 -0500

    worked on server for edithome

--- server/api/homePageImages.js
+++ server/api/homePageImages.js
@@ -15,4 +15,39 @@ router.get('/', async (req, res, next) => {
   }
 })
 
+router.post('/', async (req, res, next) => {
+  try {
+    const newHomepageImage = await HomePageImage.create(req.body)
+
+    res.send(newHomepageImage)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const homePageImageId = req.params.id
+    const updateHomePageImage = await HomePageImage.findByPk(homePageImageId)
+    res.send(await updateHomePageImage.update(req.body))
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const homePageImageId = req.params.id
+    const deleteHomePageImage = await HomePageImage.findByPk(homePageImageId)
+
+    if (!deleteHomePageImage) {
+      res.sendStatus(404)
+    }
+    await deleteHomePageImage.destroy()
+    res.send(deleteHomePageImage)
+  } catch (error) {
+    next(error)
+  }
+})
+
 module.exports = router
