10d7d253873d508d6a26992d37042147dd129bff
dahn0209 <dahn0209@gmail.com>
2022-11-19 16:50:40 -0500

    commit

---
+++ client/components/editHome.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import {
+  fetchHomepageImages,
+  deleteHomePageImageThunk
+} from '../store/homePageImages'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+// import AddProductForm from './AddProductForm'
+import './editHome.css'
+
+export class EditHome extends React.Component {
+  componentDidMount() {
+    this.props.fetchHomePageImages()
+  }
+
+  render() {
+    const homePageImages = this.props.homePageImages
+    if (homePageImages.length === 0) {
+      return (
+        <section id="noImagePresentEditHomeImages">
+          <h1>No Images present! Please upload new images!</h1>
+        </section>
+      )
+    }
+    return (
+      <section className="editHomePageImagesContainer">
+        {homePageImages.map(homePageImage => {
+          if (!homePageImage.description) {
+            return (
+              <div
+                className="editHomePageImagesGridItem"
+                key={homePageImage.id}
+              >
+                <img src={homePageImage.imageUrl} />
+                <div className="editHomePageImagesEditDelete">
+                  <Link to={`/homePageImages/${homePageImage.id}/edit`}>
+                    <button type="button">Edit</button>
+                  </Link>
+                  <button
+                    id="deleteBtn"
+                    type="button"
+                    onClick={() =>
+                      this.props.deleteHomePageImageThunk(homePageImage)
+                    }
+                  >
+                    Remove
+                  </button>
+                </div>
+              </div>
+            )
+          }
+          return (
+            <div className="editHomePageImagesGridItem" key={homePageImage.id}>
+              <img src={homePageImage.imageUrl} />
+              <div className="editHomePageImagesDescription">
+                <span>{homePageImage.description}</span>
+              </div>
+              <div className="editHomePageImagesEditDelete">
+                <Link to={`/homePageImages/${homePageImage.id}/edit`}>
+                  <button type="button">Edit</button>
+                </Link>
+                <button
+                  type="button"
+                  id="deleteBtn"
+                  onClick={() =>
+                    this.props.deleteHomePageImageThunk(homePageImage)
+                  }
+                >
+                  Remove
+                </button>
+              </div>
+            </div>
+          )
+        })}
+      </section>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    homePageImages: state.homePageImages
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchHomePageImages: () => dispatch(fetchHomepageImages()),
+    deleteHomePageImageThunk: homePageImageId =>
+      dispatch(deleteHomePageImageThunk(homePageImageId))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditHome)
