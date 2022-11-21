41b9c0c9f42a121b171efe173ee32c2d31a0bf8c
dahn0209 <dahn0209@gmail.com>
2022-11-21 18:19:22 -0500

    working on addNewImage

---
+++ client/components/addNewHomeImage.js
@@ -0,0 +1,76 @@
+import React from 'React'
+import {createHomePageImage} from '../store/homePageImages'
+import {connect} from 'react-redux'
+
+const defaultState = {
+  imageUrl: '',
+  description: ''
+}
+
+class AddHomePageImageForm extends React.Component {
+  constructor() {
+    super()
+    this.state = defaultState
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    this.props.createHomePageImage({...this.state})
+    this.setState(defaultState)
+  }
+
+  render() {
+    const {imageUrl, description} = this.state
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <h1>New Image Details</h1>
+
+          <label htmlFor="imageUrl">Image</label>
+          <input
+            type="text"
+            name="imageUrl"
+            value={imageUrl}
+            placeholder="imageUrl"
+            accept="image/webp, image/png image/jpeg"
+            onChange={this.handleChange}
+          />
+
+          <label htmlFor="description">Description</label>
+          <input
+            type="text"
+            name="description"
+            value={description}
+            placeholder="Product Description"
+            onChange={this.handleChange}
+          />
+          <button type="submit">Add Image</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+const mapState = state => {
+  return {
+    newProduct: state.products
+  }
+}
+
+const mapDispatch = dispatch => {
+  return {
+    createHomePageImage: homePageImage =>
+      dispatch(createHomePageImage(homePageImage))
+  }
+}
+
+export default connect(mapState, mapDispatch)(AddHomePageImageForm)
