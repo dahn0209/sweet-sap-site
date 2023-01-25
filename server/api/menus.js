b6cc247cded2878e878795e6d34a5288659c67f8
dahn0209 <dahn0209@gmail.com>
2023-01-24 19:31:31 -0500

    finish menu api Crud

--- server/api/menus.js
+++ server/api/menus.js
@@ -29,4 +29,63 @@ router.get('/', async (req, res, next) => {
   }
 })
 
+router.get('/:menuId', async (req, res, next) => {
+  try {
+    const menu = await Menu.findByPk(req.params.menuId)
+    console.log('get sing menu Id backend=>', menu)
+    res.json(menu)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/', upload.single('imageUrl'), async (req, res, next) => {
+  try {
+    console.log('req.file->', req.file)
+    let imageUrl = req.file.originalname
+    let newImg = `./menu/${imageUrl}`
+    const newMenu = await Menu.create({
+      description: req.body.description,
+      imageUrl: newImg
+    })
+    res.send(newMenu)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.put('/:menuId', upload.single('imageUrl'), async (req, res, next) => {
+  try {
+    let imageUrl = req.file.originalname
+    console.log('api imageURL put=>', imageUrl)
+    let newImg = `./menu/${imageUrl}`
+    const menuId = req.params.menuId
+    const updateMenu = await Menu.findByPk(menuId)
+    console.log('updateMenu  API=>', updateMenu)
+    res.send(
+      await updateMenu.update({
+        description: req.body.description,
+        imageUrl: newImg
+      })
+    )
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete('/:menuId', async (req, res, next) => {
+  try {
+    const menuId = req.params.homePageImageId
+    const deleteMenu = await Menu.findByPk(menuId)
+
+    if (!deleteMenu) {
+      res.sendStatus(404)
+    }
+    await deleteMenu.destroy()
+    res.send(deleteMenu)
+  } catch (error) {
+    next(error)
+  }
+})
+
 module.exports = router
