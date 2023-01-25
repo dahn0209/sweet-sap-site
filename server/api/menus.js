76c1eebea9be144056f1fcb8380c1c67510269a4
dahn0209 <dahn0209@gmail.com>
2023-01-24 19:11:43 -0500

    working on api for menus

--- server/api/menus.js
+++ server/api/menus.js
@@ -1,7 +1,21 @@
 const router = require('express').Router()
 const {Menu} = require('../db/models')
-
+const multer = require('multer')
 // <-- assumes main route to products set up with app.use in index.js -->  //
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    ///destination:where file is stgored
+    cb(null, 'public/menu/')
+  },
+  filename: function(req, file, cb) {
+    ///defines how file is named
+    console.log('file->', file)
+    cb(null, file.originalname)
+  }
+})
+
+const upload = multer({storage: storage})
 
 router.get('/', async (req, res, next) => {
   try {
